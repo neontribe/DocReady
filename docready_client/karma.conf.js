5e972431230d44e38bf45ae5b59349cafe191e42
Rupert Redington <rupert@neontribe.co.uk>
2013-05-08 09:43:25 +0100

    Include PhantomJS

--- docready_client/karma.conf.js
+++ docready_client/karma.conf.js
@@ -38,6 +38,8 @@ logLevel = LOG_INFO;
 // enable / disable watching file and executing tests whenever any file changes
 autoWatch = false;
 
+
+process.env['PHANTOMJS_BIN'] = './node_modules/.bin/phantomjs';
 // Start these browsers, currently available:
 // - Chrome
 // - ChromeCanary
@@ -46,7 +48,7 @@ autoWatch = false;
 // - Safari (only Mac)
 // - PhantomJS
 // - IE (only Windows)
-browsers = ['Chrome'];
+browsers = ['PhantomJS'];
 
 // If browser does not capture in given timeout [ms], kill it
 captureTimeout = 5000;
