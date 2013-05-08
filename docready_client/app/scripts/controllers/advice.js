1ca5c895d49692baf1e7bf445d134d0f2be76bdd
Rupert Redington <rupert@neontribe.co.uk>
2013-05-08 08:59:41 +0100

    Mockup for advice controller

--- docready_client/app/scripts/controllers/advice.js
+++ docready_client/app/scripts/controllers/advice.js
@@ -2,17 +2,32 @@
 
 angular.module('docreadyClientApp')
   .controller('AdviceCtrl', function ($scope) {
-    $scope.topics = ['Topic 1', 'Topic 2'];
+    $scope.topics = [
+        {
+          title: 'Topic 1',
+          slug: 'topic-1',
+          weight: 1.0
+        },
+        {
+          title: 'Topic 2',
+          slug: 'topic-2',
+          weight: 2.0
+        }
+      ];
     $scope.items = [
         {
             title: 'Item 1',
+            slug: 'item-1',
             body: 'Blah',
-            topic: 'Topic 1'
+            topic: 'Topic 1',
+            weight: 1.0
           },
           {
             title: 'Item 2',
+            slug: 'item-2',
             body: 'Blah',
-            topic: 'Topic 2'
+            topic: 'Topic 2',
+            weight: 2.0
           }
         ];
   });
