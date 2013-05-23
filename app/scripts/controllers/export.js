ec7769a977c7207b75f37206d3cbc10656b989fc
Rupert Redington <rupert@neontribe.co.uk>
2013-05-23 15:18:00 +0100

    Stub out buttons on Export screen

--- app/scripts/controllers/export.js
+++ app/scripts/controllers/export.js
@@ -1,6 +1,9 @@
 'use strict';
 
 angular.module('docready')
-  .controller('ExportCtrl', function ($scope, symptomService) {
+  .controller('ExportCtrl', function ($scope, symptomService, $window) {
     $scope.symptoms = symptomService.symptoms;
+    $scope.print = function(){
+      $window.print();
+    };
   });
