43650634a4b3aefd6552a9827a92a73935cb023b
Rupert Redington <rupert@neontribe.co.uk>
2013-05-23 16:58:24 +0100

    Email sender 101

--- app/scripts/controllers/export.js
+++ app/scripts/controllers/export.js
@@ -3,6 +3,15 @@
 angular.module('docready')
   .controller('ExportCtrl', function ($scope, symptomService, $window) {
     $scope.symptoms = symptomService.symptoms;
+    $scope.prepareMail = function(){
+      $scope.email = {
+        recipient: '',
+        checklist: _.chain($scope.symptoms).where({selected: true}).pluck('title').value()
+      };
+    };
+    $scope.sendEmail = function(){
+      console.log($scope.email);
+    };
     $scope.print = function(){
       $window.print();
     };
