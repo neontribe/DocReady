3d6306adbd0e9fe4b1003a08fc37d50e193cd1ba
Rupert Redington <rupert@neontribe.co.uk>
2013-05-24 13:39:25 +0100

    MOAR Tests

--- test/spec/controllers/export.js
+++ test/spec/controllers/export.js
@@ -7,11 +7,15 @@ describe('Controller: ExportCtrl', function () {
 
   var ExportCtrl,
     $httpBackend,
+    timeout,
+    $window,
     scope;
 
   // Initialize the controller and a mock scope
-  beforeEach(inject(function ($controller, $rootScope, _$httpBackend_, mocks) {
+  beforeEach(inject(function ($controller, $rootScope, _$httpBackend_, mocks, $timeout, _$window_) {
     $httpBackend = _$httpBackend_;
+    timeout = $timeout;
+    $window = _$window_;
     mocks.registerMocks($httpBackend);
     scope = $rootScope.$new();
     ExportCtrl = $controller('ExportCtrl', {
@@ -22,5 +26,46 @@ describe('Controller: ExportCtrl', function () {
   it('should attach a list of symptoms to the scope', function () {
     $httpBackend.flush();
     expect(scope.symptoms.length).toBeGreaterThan(2);
+  });
+
+  it('should have a prepareMail function which toggles the Email resource', function () {
+    $httpBackend.flush();
+    expect(scope.email).toBeFalsy();
+    scope.prepareMail();
+    expect(scope.email).toBeTruthy();
+    scope.prepareMail();
+    expect(scope.email).toBeFalsy();
+  });
+
+  it('should have a prepareMail function which serializes the checklist into the Email', function () {
+    $httpBackend.flush();
+    scope.symptoms[0].selected = true;
+    scope.symptoms[1].selected = true;
+    scope.prepareMail();
+    expect(scope.email.checklist.length).toBe(2);
+    expect(scope.email.checklist[1]).toEqual(scope.symptoms[1].title);
+  });
+
+  it('should have a sendEmail function which posts the email', function () {
+    $httpBackend.flush();
+    scope.symptoms[0].selected = true;
+    scope.symptoms[1].selected = true;
+    scope.prepareMail();
+    expect(scope.email).toBeTruthy();
+    $httpBackend.expectPOST('/api/email');
+    scope.sendEmail();
+    $httpBackend.flush();
+    expect(scope.email.state).toBe('sent');
+    timeout.flush();
+    expect(scope.email).toBeFalsy();
+    $httpBackend.verifyNoOutstandingExpectation();
+    $httpBackend.verifyNoOutstandingRequest();
+  });
+
+  it('should have a print function which calls $window.print', function () {
+    $httpBackend.flush();
+    spyOn($window, 'print');
+    scope.print();
+    expect($window.print).toHaveBeenCalled();
   });
 });
