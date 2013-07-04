39e6c1b6b5f66d18a6c2737e893c6eac50a521d8
Rupert Redington <rupert@neontribe.co.uk>
2013-07-04 13:46:28 +0100

    Fix selection of active advice items

--- app/scripts/controllers/advice.js
+++ app/scripts/controllers/advice.js
@@ -2,49 +2,33 @@
 
 angular.module('docready')
   .controller('AdviceCtrl', function ($scope, adviceService, $routeParams, $location) {
-    $scope.activeTopic = null;
-    adviceService.topics.$then(function(data){
-      $scope.topics = data.resource;
-      $scope.setActiveTopic();
-    });
-    $scope.activeItem = null;
-    adviceService.items.$then(function(data){
-      $scope.items = data.resource;
-      $scope.initialItem();
-    });
-
-    $scope.setActiveTopic = function(topic) {
-      if (topic) {
-        $scope.activeTopic = topic;
-      } else {
-        if ($routeParams.topic) {
-          $scope.activeTopic = _.findWhere($scope.topics, {slug: $routeParams.topic});
-        } else {
-          $scope.activeTopic = $scope.topics[0];
+    // Process income advice item/topic data
+    function initData(data, type) {
+      if ($routeParams[type]) {
+        // Set the active entry, if any
+        $scope[type] = _.findWhere(data, {slug: $routeParams[type]});
+        if ($scope[type]) {
+          // since we're directly addressing a topic/item we'll move it to the top of the list!
+          data.splice(_.indexOf(data, $scope[type]), 1);
+          data.unshift($scope[type]);
         }
       }
-      $location.search('topic', $scope.activeTopic.slug);
-    };
+      return data;
+    }
 
-    $scope.initialItem = function() {
-      if ($routeParams.item) {
-        $scope.activeItem = _.findWhere($scope.items, {slug: $routeParams.item});
-        // since we're directly addressing an item we'll move it to the top of the list!
-        $scope.elevateItem($scope.activeItem);
-      }
-    };
+    adviceService.topics.$then(function(data){
+      $scope.topics = initData(data.resource, 'topic');
+    });
+    adviceService.items.$then(function(data){
+      $scope.items = initData(data.resource, 'item');
+    });
 
     $scope.setItem = function(slug) {
       $location.search('item', slug);
     };
 
-    $scope.elevateItem = function(item) {
-      var index, sp;
-      index = _.indexOf($scope.items, item);
-      if (item && index !== -1) {
-        sp = $scope.items.splice(index, 1);
-        $scope.items.unshift(sp[0]);
-      }
+    $scope.setTopic = function(slug) {
+      $location.search('topic', slug);
     };
   });
 
--- test/spec/controllers/advice.js
+++ test/spec/controllers/advice.js
@@ -33,17 +33,6 @@ describe('Controller: AdviceCtrl', function () {
     expect(scope.items.length).toBe(4);
   });
 
-  it('should activate the first topic by default', function(){
-    $httpBackend.flush();
-    expect(scope.activeTopic).toEqual(scope.topics[0]);
-  });
-
-  it('should provide a methiod to set an active topic', function(){
-    $httpBackend.flush();
-    scope.setActiveTopic(scope.topics[2]);
-    expect(scope.activeTopic).toEqual(scope.topics[2]);
-  });
-
   it('should respect the active topic set in routeParams', function(){
     var routeParams = {topic: 'topic-2'};
     AdviceCtrl = controller('AdviceCtrl', {
@@ -51,7 +40,7 @@ describe('Controller: AdviceCtrl', function () {
       $routeParams: routeParams
     });
     $httpBackend.flush();
-    expect(scope.activeTopic.slug).toEqual('topic-2');
+    expect(scope.topic.slug).toEqual('topic-2');
   });
 
   it('should respect the active item set in routeParams', function(){
@@ -61,7 +50,7 @@ describe('Controller: AdviceCtrl', function () {
       $routeParams: routeParams
     });
     $httpBackend.flush();
-    expect(scope.activeItem.slug).toEqual('item-3');
+    expect(scope.item.slug).toEqual('item-3');
   });
 
   it('should elevate the active item set in routeParams to the front of the items array', function(){
@@ -89,5 +78,11 @@ describe('Controller: AdviceCtrl', function () {
     $httpBackend.flush();
     scope.setItem('test-slug');
     expect(location.search().item).toEqual('test-slug');
+  });
+
+  it('should provide a setTopic method which sets the topic in the location search', function(){
+    $httpBackend.flush();
+    scope.setTopic('test-slug');
+    expect(location.search().topic).toEqual('test-slug');
   });
 });
