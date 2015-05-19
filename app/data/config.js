2d3f374923852f430a447f427d46362fedfa87c4
Heydon <heydon@heydonworks.com>
2015-05-19 17:23:15 +0100

    new colours

--- app/data/config.js
+++ app/data/config.js
@@ -1,7 +1,7 @@
 angular.module('docready').constant('custom_config', {
-  "ga_code": "UA-42308316-2",
-  "app_name": "Doc Ready",
-  "baseUrl": "http://www.docready.org",
-  "mailer_from":  "hello@docready.org",
-  "mailer_subject": "Doc Ready Checklist"
+  "ga_code": "pending",
+  "app_name": "CAMHSReady",
+  "baseUrl": "http://camhsready.herokuapp.com",
+  "mailer_from":  "hello@camhsready.org",
+  "mailer_subject": "CAMHSReady Checklist"
 });
