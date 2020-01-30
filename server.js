e1f908a6505f275c043a9c056a56354970417c37
zii-67 <zi@neontribe.co.uk>
2020-01-30 13:56:47 +0000

    version updated

--- server.js
+++ server.js
@@ -110,7 +110,7 @@ app.get('/static/client/index.html', function(req, res){
 /**
  * Startup
  */
-var server = app.listen("localhost", 3000, function () {
+var server = app.listen(process.env.PORT || 3000, function () {
   var host = server.address().address;
   var port = server.address().port;
   console.log('Listening at http://%s:%s', host, port);
